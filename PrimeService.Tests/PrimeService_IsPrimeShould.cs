8869b514ba2a59a7cd1834838ec9a3d2dc35450f
cristianoandrad <cristianocontabil@gmail.com>
2021-09-09 16:30:45 -0300

    first commit

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            bool result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+    }
+}
---
+++ unit-testing-using-mstest/PrimeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
